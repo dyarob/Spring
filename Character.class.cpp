d0606d3363b1d1146c596a96463460b169f1f8b2
Adelie <dyarob@gmail.com>
2020-03-14 00:02:11 +0100

    first dialog

--- Character.class.cpp
+++ Character.class.cpp
@@ -2,5 +2,16 @@
 
 void	talk(Character pl, Character anissa) {
 if (std::abs((int)(pl.y-anissa.y))+std::abs((int)(pl.x-anissa.x)) < 3) {
+	WINDOW	*w = newwin(5, 50, LINES/2+12, COLS/2-25);
+	box(w, 0, 0);
+	mvwaddstr(w, 2, 5, "Anissa: Welcome to Springs Isle!");
+	wrefresh(w);
+	char	c;
+	while((c=getch())==ERR);
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 50; j++)
+			mvwaddch(w, i, j, ' ');
+	wrefresh(w);
+	delwin(w);
 }
 }
--- inc/Character.class.hpp
+++ inc/Character.class.hpp
@@ -1,6 +1,10 @@
 #ifndef CHARACTER_HPP
 # define CHARACTER_HPP
 
+# include <curses.h>
+# include "Character.class.hpp"
+# include <cstdlib>	// abs
+
 class	Character {
 public:
 unsigned char	c;
@@ -8,5 +12,7 @@ unsigned char	c;
 unsigned int	x;
 unsigned int	y;
 };
+
+void	talk(Character pl, Character anissa);
 
 #endif
